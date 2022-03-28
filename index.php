5b4e0ae890bced9ae1d38fe250192e49266ea49a
99062751 <70890704+99062751@users.noreply.github.com>
2022-03-28 08:45:21 +0200

    zondag 27 maart
    update aanval helemaal klaar en loop gemaakt die het uitvoert alleen de timing fixen en nieuwe bestanden gebruiken dan is alles klaar

--- index.php
+++ index.php
@@ -11,25 +11,24 @@
     $pickachu= new Pokemon("Pickachu", "Lightning", 100, [$atck1_pichachu , $atck2_pichachu], "Lightning", "Poison");
     $enemy= new Pokemon("Magikarp", "Water", 150, [$atck1_magikarp , $atck2_magikarp], "Water", "Ok");
 
-
     echo "$pickachu->name vs $enemy->name! <br>";
-    echo $pickachu->execute_attack($pickachu->Attacks, 1, $enemy). "<br>";
-    echo $enemy->printHealth($enemy). "<br>";
-    echo $enemy->execute_attack($enemy->Attacks, 1, $pickachu). "<br>";
-    echo $pickachu->printHealth($pickachu). "<br>";
-
-    echo "<br>";
-
-    echo $pickachu->execute_attack($pickachu->Attacks, 0, $enemy). "<br>";
-    echo $enemy->printHealth($enemy). "<br>";
-    echo $enemy->execute_attack($enemy->Attacks, 0, $pickachu). "<br>";
-    echo $pickachu->printHealth($pickachu). "<br>";
+    while (!(Pokemon::$hp_enemy <= 0) || !(Pokemon::$hp_pickachu <= 0)) {
+        echo $pickachu->execute_attack($pickachu->Attacks, rand(0, (count($pickachu->Attacks) - 1)), $enemy). "<br>";
+        echo $enemy->printHealth($enemy). "<br>";
+        if(!(Pokemon::$hp_enemy <= 0) || !(Pokemon::$hp_pickachu <= 0)){
+            echo $enemy->execute_attack($enemy->Attacks, rand(0, (count($pickachu->Attacks) - 1)), $pickachu). "<br>";
+            echo $pickachu->printHealth($pickachu). "<br>";
+        }
+    }
     
 
-    // echo $pickachu->execute_attack([$atck1_pichachu , $atck2_pichachu], 0, $enemy). "<br>";
+    // echo "<br>";
+
+    // echo $pickachu->execute_attack($pickachu->Attacks, rand(0, (count($pickachu->Attacks) - 1)), $enemy). "<br>";
     // echo $enemy->printHealth($enemy). "<br>";
-    // echo $enemy->execute_attack([$atck1_magikarp , $atck2_magikarp], 0, $pickachu). "<br>";
-    // echo $enemy->printHealth($pickachu). "<br>";
+    // echo $enemy->execute_attack($enemy->Attacks, rand(0, (count($pickachu->Attacks) - 1)), $pickachu). "<br>";
+    // echo $pickachu->printHealth($pickachu). "<br>";
+    
     
     
 
--- pokemon_builder.php
+++ pokemon_builder.php
@@ -8,7 +8,8 @@
         public $Weakness;
         public $Resistance;
         public $damage;
-        public static $hp;
+        public static $hp_enemy;
+        public static $hp_pickachu;
 
         public function __construct($name, $EnergyType, $hitpoints, $Attacks, $Weakness, $Resistance)
         {
@@ -19,34 +20,54 @@
             $this->damage= $attackdamage;
             $this->Weakness = $Weakness;
             $this->Resistance = $Resistance;
-            Pokemon::$hp= $hitpoints;
+            Pokemon::$hp_enemy= $hitpoints;
+            Pokemon::$hp_pickachu= 100;
         }
 
         public function execute_attack($atcks, $attacknumber, $pokemon){
-            if($pokemon->name == "Magikarp"){
                 $atck=  $atcks[$attacknumber];
                 if($pokemon->Resistance == $atck->type){
-                    $newdamage= ($atck->damage) - ($atck->Resistance);
-                    Pokemon::$hp= Pokemon::$hp - $newdamage;
-                    var_dump(Pokemon::$hp - $newdamage);
-                    
+                    if($pokemon->name == "Magikarp"){
+                        $newdamage= ($atck->damage) - ($atck->Resistance);
+                        Pokemon::$hp_enemy= Pokemon::$hp_enemy - $newdamage;
+                    }else{
+                        $newdamage= ($atck->damage) - ($atck->Resistance);
+                        Pokemon::$hp_pickachu= Pokemon::$hp_pickachu - $newdamage;
+                    }
                     return $pokemon->name . " took " . $newdamage ." ". " " . $atck->type ." damage!";
                 }elseif($pokemon->Weakness == $atck->type){
-                    $newdamage= $atck->damage + $atck->Weakness;
-                    Pokemon::$hp= Pokemon::$hp - $newdamage;
-                    var_dump("eeeee ". Pokemon::$hp - $newdamage);
+                    if($pokemon->name == "Magikarp"){
+                        $newdamage= ($atck->damage) + ($atck->Weakness);
+                        Pokemon::$hp_enemy= Pokemon::$hp_enemy - $newdamage;
+                    }else{
+                        $newdamage= ($atck->damage) - ($atck->Weakness);
+                        Pokemon::$hp_pickachu= Pokemon::$hp_pickachu - $newdamage;
+                    }
                 return $pokemon->name . " took " . $newdamage . " " . $atck->type ." damage!";
                 }else{
-                    Pokemon::$hp= Pokemon::$hp - $atck->damage;
-                    var_dump(Pokemon::$hp - $atck->damage);
-                        return $pokemon->name . " took " . $atck->damage . " " . $atck->type ." damage!";
+                    if($pokemon->name == "Magikarp"){
+                        Pokemon::$hp_enemy= Pokemon::$hp_enemy - $atck->damage;
+                    }else{
+                        $newdamage= ($atck->damage) - ($atck->Weakness);
+                        Pokemon::$hp_pickachu= Pokemon::$hp_pickachu - $atck->damage;
+                    }
+                    return $pokemon->name . " took " . $atck->damage . " " . $atck->type ." damage!";
                 }
-            }else{
-                return "nope";
-            }
         }
         public static function printHealth($pokemon){
-            return $pokemon->name. " has only " . Pokemon::$hp. " hitpoints left!";
+            if($pokemon->name == "Magikarp"){
+                if($pokemon->hitpoints <= 0){
+                    return $pokemon->name . " Has been defeated!!"; 
+                }else{
+                    return $pokemon->name. " has only " . Pokemon::$hp_enemy. " hitpoints left!";
+                }
+            }else{
+                if($pokemon->hitpoints <= 0){
+                    return $pokemon->name . " Has been defeated!!";
+                }else{
+                    return $pokemon->name. " has only " . Pokemon::$hp_pickachu. " hitpoints left!!";
+                }
+            }
         }
     }
 
